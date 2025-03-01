f17636a35c9aca548751760c511f3c7d1f10b80e
BanaPham <phamlananha1anh@gmail.com>
2025-03-01 18:07:16 +0700

    update

--- src/login.js
+++ src/login.js
@@ -4,26 +4,26 @@ function LogIn()
 {
     document.querySelector("#content").innerHTML = `
         <div id="login" class="flex justify-center items-center min-h-screen">
-            <form action="" class="flex flex-col bg-gray-200 w-sm p-6 rounded-lg">
+            <form id="form-login" action="" class="flex flex-col bg-gray-200 w-sm p-6 rounded-lg">
                 <h2 class="font-bold text-3xl flex justify-center mb-5">Đăng nhập</h2>
                 <p class="mb-1">Tên đăng nhập</p>
-                <input class="rounded-sm p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" type="email" placeholder="Email">
+                <input id="email" class="rounded-sm p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" type="email" placeholder="Email">
                 <p class="mt-3 mb-1">Mật khẩu</p>
                 <input class="rounded-sm p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" type="password" placeholder="Password">
-                <button type="submit" class="w-full bg-green-600 text-white py-2 px-4 mt-5 cursor-pointer rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Đăng Nhập</button>
+                <button id="pass" type="submit" class="w-full bg-green-600 text-white py-2 px-4 mt-5 cursor-pointer rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Đăng Nhập</button>
                 <a href="#" class="text-base flex justify-center mt-2 text-blue-600 hover:text-indigo-500">Quên mật khẩu?</a>
             </form>
         </div>
     `
-    
-    const email = "test@test.com";
-    const password = "test";
-    const auth = {
-        email,
-        password
-    };
-    this.addEventListener("submit", function (x) {
-        fetch('http://localhost:3000/auth/login', {
+    const Login = document.getElementById("form-login");
+    Login.addEventListener("submit", function () {
+        const email = document.getElementById("email");
+        const password = document.getElementById("pass");
+        const auth = {
+            email,
+            password
+        };
+        const data = fetch('https://auth-wit.vercel.app/auth/login', {
             method: 'POST',
             body: JSON.stringify(auth),
             headers: {
@@ -42,18 +42,8 @@ function LogIn()
                 window.location.href = "/home"
             })
             .catch((error) => {
-                alert("Đăng nhập thất bại");
+                alert(error);
             });
-        const items = document.querySelectorAll('.icon');
-        if (items.length > 0) {
-            items[0].classList.add('show');
-        }
-        items.forEach(btn => {
-            btn.addEventListener('click', () => {
-                items.forEach(item => item.classList.remove('show'));
-                btn.classList.add('show');
-            });
-        });
     });
 }
 
@@ -62,13 +52,6 @@ function Home()
     document.querySelector("#content").innerHTML = `
         <strong>Tạm ẩn</strong>
     `
-    const items = document.querySelectorAll('.icon');
-    items.forEach(btn => {
-        btn.addEventListener('click', () => {
-            items.forEach(item => item.classList.remove('show'));
-            btn.classList.add('show');
-        });
-    });
 }
 
 export default LogIn
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Navigo from "navigo";
-import Home, {LogIn} from "./login.js"
+import LogIn, {Home} from "./login.js"
 
 const router = new Navigo('/');
-
-document.addEventListener("click", (event) => {
-    const button = event.target.closest("button"); 
-    if (button) {
-        const link = button.querySelector("a"); 
-        if (link) {
-            event.preventDefault(); 
-            router.navigate(link.getAttribute("href")); 
-        }
-    }
-});
 
 router.on("/", function(){
     LogIn()
